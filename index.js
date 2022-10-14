2e134175fa0e941dc71fcddbfa63320d6938086e
Staci Rosales <stacileep2@gmail.com>
2022-10-14 10:42:52 -0500

    commit tree e83e0cd04dd0c4aa9b2c62e0607fcfc0aa6a1011

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter((possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array, name){
+    return array.filter((possibleMatch) => possibleMatch[0] === name[0]);
+}
+
+// //* `matchName` - This function takes an array of `driver` objects and a `string`
+// as arguments. Each `driver` object has two properties: `name` and `hometown`.
+// The function should return each element whose `name` property matches the
+// provided `string` argument.
+
+
+
+function matchName(array, name) {
+    return array.filter((possibleMatch) => possibleMatch.name === name);
+}
